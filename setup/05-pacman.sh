f63e1ff763a6efc9b623cd410edb8afbafe75d63
Jamee Kim <jameekim@nodaplife.me>
2019-06-25 20:15:17 +0900

    sync install scripts

--- setup/05-pacman.sh
+++ setup/05-pacman.sh
@@ -3,12 +3,13 @@ INSTALL_CMD="sudo pacman -S --needed --noconfirm"
 # sync and update packages
 sudo pacman -Syu --noconfirm
 
-install_utils=(openssh wget alsa-utils firefox)
+install_utils=(openssh wget alsa-utils pulseaudio pulseaudio-alsa pasystray firefox)
 install_zsh=(zsh vim terminus-font tmux)
 install_x=(xorg xcompmgr xmonad-contrib xmobar xdotool xorg-xmessage xorg-xinit rxvt-unicode rofi stalonetray sddm)
 install_fonts=(adobe-source-han-sans-kr-fonts adobe-source-han-serif-kr-fonts)
+install_sns=(telegram-desktop)
 
-installs=(install_utils install_zsh install_x install_fonts)
+installs=(install_utils install_zsh install_x install_fonts install_sns)
 
 $INSTALL_CMD ${installs[@]}
 
@@ -26,3 +27,10 @@ fi
 if [[ ! -e $HOME/.config ]] ; then
     mkdir -p $HOME/.config
 fi
+
+unset install_utils
+unset install_zsh
+unset install_x
+unset install_fonts
+unset install_sns
+unset installs
