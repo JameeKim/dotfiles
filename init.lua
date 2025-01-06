93307774cb07533c1e1828f98bc3147541aae121
Jamee Kim <jameekim@nodaplife.me>
2025-01-06 17:37:25 -0500

    Workaround for nvim v0.10.3 bug

--- init.lua
+++ init.lua
@@ -1,1 +1,7 @@
+-- A workaround for a bug in Neovim v0.10.3 where :Inspect throws an error.
+-- This is caused by Lua code in `vim.show_pos()` using `vim.hl` which have been
+-- renamed to `vim.highlight`.
+-- See: https://github.com/neovim/neovim/issues/31675#issuecomment-2558405042
+vim.hl = vim.highlight
+
 require("jameekim")
