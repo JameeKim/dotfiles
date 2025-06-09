1cae47b9eb4319790c3dc592bed09288082624ae
Jamee Kim <jameekim@nodaplife.me>
2025-06-09 17:29:32 -0400

    fix(lsp.kotlin_language_server): set proper `storagePath` in init opts
    The config from nvim-lspconfig sets wrong `storagePath` which can be
    `nil` sometimes. This results in `init_options` being an empty table,
    which is then encoded as an array in JSON, crashing the server in the
    end. This commit sets initial `storagePath` to an empty string to
    prevent `init_options` being encoded as an array and later sets the
    proper value in `before_init` according to the `root_dir` of the lsp
    client.

--- after/lsp/kotlin_language_server.lua
+++ after/lsp/kotlin_language_server.lua
@@ -26,4 +26,21 @@ return {
   cmd_env = {
     ["JAVA_HOME"] = "/usr/lib/jvm/java-17-openjdk",
   },
+  init_options = {
+    -- The default value in nvim-lspconfig returns `nil` if `root_markers` are
+    -- not found for the buffer that was open when the config file was read.
+    -- This results in the `init_options` being `{}`, which is encoded as an
+    -- empty array, causing invalid JSON error on the server and crashing it.
+    -- Since we want the database to reside in project root, we assign this
+    -- value in `before_init` dynamically. This empty string is just here to
+    -- overwrite the value from nvim-lspconfig.
+    storagePath = "",
+  },
+  before_init = function(params, config)
+    if config.root_dir then
+      params.initializationOptions.storagePath = config.root_dir
+    else
+      params.initializationOptions.storagePath = vim.fn.stdpath("cache")
+    end
+  end,
 }
