27066c24b2f3cb7b280e01abd742eebc06b1431b
Jamee Kim <jameekim@nodaplife.me>
2025-06-09 16:54:29 -0400

    Adjust lazy.nvim config
    Calling `setup()` with a single argument confuses lua_ls and gives type
    mismatched warning. Therefore, separate the spec and config parts.

--- lua/jameekim/init.lua
+++ lua/jameekim/init.lua
@@ -3,44 +3,41 @@ require("jameekim.lazy")
 require("jameekim.options")
 require("jameekim.keymaps")
 
-require("lazy").setup({
-  spec = {
+require("lazy").setup(
+  {
     { import = "jameekim.plugins" },
   },
-  defaults = {
-    lazy = true,
-  },
-  install = {
-    colorscheme = { "gapstyle", "gruvbox", "desert" },
-  },
-  checker = {
-    enabled = true,
-    frequency = 3600,
-  },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        --"matchit",
-        --"matchparen",
-        --"netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
+  {
+    defaults = {
+      lazy = true,
+    },
+    install = {
+      colorscheme = { "gapstyle" },
+    },
+    checker = {
+      enabled = true,
+      frequency = 3600,
+    },
+    performance = {
+      rtp = {
+        disabled_plugins = {
+          "gzip",
+          --"matchit",
+          --"matchparen",
+          --"netrwPlugin",
+          "tarPlugin",
+          "tohtml",
+          "tutor",
+          "zipPlugin",
+        },
       },
     },
-  },
-  ---@diagnostic disable-next-line: assign-type-mismatch
-  dev = {
-    path = "~/dev",
-    patterns = {},
-  },
-  rocks = {
-    enabled = false,
-  },
-  profiling = {
-    loader = false,
-    require = false,
-  },
-})
+    dev = {
+      path = "~/dev",
+      patterns = { "jameekim" },
+    },
+    rocks = {
+      enabled = false,
+    },
+  }
+)
