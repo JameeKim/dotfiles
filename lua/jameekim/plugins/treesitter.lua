9641ac6bd3518e054c6f3d8647bffa77c9e3d903
Jamee Kim <jameekim@nodaplife.me>
2025-06-09 16:34:00 -0400

    Update nvim-treesitter to branch main

--- lua/jameekim/plugins/treesitter.lua
+++ lua/jameekim/plugins/treesitter.lua
@@ -1,195 +1,257 @@
 -- Settings related to treesitter
 
+local augroup = vim.api.nvim_create_augroup("jameekim.treesitter", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup,
+  callback = function(args)
+    local lang = vim.treesitter.language.get_lang(args.match)
+    if lang == "" then
+      return
+    end
+    local ok, ts_config = pcall(require, "nvim-treesitter.config")
+    if not ok then
+      return
+    end
+    if vim.list_contains(ts_config.get_installed("parsers"), lang) then
+      vim.treesitter.start(args.buf, lang)
+      return
+    end
+    if not require("nvim-treesitter.parsers")[lang] then
+      return
+    end
+    local choice = vim.fn.confirm(
+      "TS parser for " .. lang .. " not found. Install?",
+      "&Install\n&No",
+      1,
+      "Question"
+    )
+    if choice == 1 then
+      require("nvim-treesitter").install(lang):await(function (err)
+        if not err then
+          vim.treesitter.start(args.buf, lang)
+        end
+      end)
+    end
+  end,
+})
+
 ---@type LazyPluginSpec[]
 return {
   {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    config = function()
-      -- Other movements not related to treesitter can also be made repeatable
-      -- using methods like:
-      -- * `make_repeatable_move_pair(next, prev)`
-      -- * `make_repeatable_move(move)`
-      -- * `set_last_move(??)`
-      local repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-      local modes = { "n", "x", "o" }
-
-      -- Repeat movement with ; and ,
-      vim.keymap.set(modes, ";", repeat_move.repeat_last_move)
-      vim.keymap.set(modes, ",", repeat_move.repeat_last_move_opposite)
-
-      -- Make builtin f, F, t, T also repeatable with ; and ,
-      -- vim.keymap.set(modes, "f", repeat_move.builtin_f)
-      -- vim.keymap.set(modes, "F", repeat_move.builtin_F)
-      -- vim.keymap.set(modes, "t", repeat_move.builtin_t)
-      -- vim.keymap.set(modes, "T", repeat_move.builtin_T)
-    end
-  },
-  {
     "nvim-treesitter/nvim-treesitter-context",
+    cmd = { "TSContext" },
     event = { "VeryLazy" },
+    ---@type TSContext.UserConfig
     opts = {
+      enable = true,
       max_lines = 3,
+      min_window_height = 8,
       line_numbers = true,
-      mode = "cursor", -- "cursor" | "topline"
+      trim_scope = "outer",
+      mode = "cursor",
+      separator = "-",
     },
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    event = { "VeryLazy" },
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    keys = {
-      { "<C-Space>", mode = { "n", "x" }, desc = "Increment selection" },
-      { "<BS>",      mode = "x",          desc = "Decrement selection" },
-    },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    -- A trick to make treesitter queries available at startup.
-    -- Copied (and rephrased the comment a little bit) from:
-    -- https://github.com/LazyVim/LazyVim/blob/1e1b68d633d4bd4faa912ba5f49ab6b8601dc0c9/lua/lazyvim/plugins/treesitter.lua#L10
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and load custom query
-      --       predicates early
-      -- This is needed because a bunch of plugins no longer
-      -- `require("nvim-treesitter")`, which no longer trigger the
-      -- **nvim-treesitter** module to be loaded in time. Luckily, the only
-      -- things that those plugins need are the custom queries, which we make
-      -- available during startup.
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
-    end,
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
     ---@type TSConfig
     ---@diagnostic disable-next-line: missing-fields
+    opts = {},
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    lazy = false,
+    ---@type TSTextObjects.UserConfig
     opts = {
-      auto_install = true,
-      ensure_installed = {
-        "lua",
-        "luadoc",
-        "markdown",
-        "markdown_inline",
-        "query",
-        "vim",
-        "vimdoc",
+      select = {
+        lookahead = true,
+        selection_modes = {
+          ["@parameter.inner"] = "v",
+          ["@parameter.outer"] = "v",
+          ["@function.inner"] = "v",
+          ["@function.outer"] = "V",
+          ["@class.inner"] = "V",
+          ["@class.outer"] = "V",
+        },
+        include_surrounding_whitespace = function(params)
+          return params.query_string:find("%.outer$") ~= nil
+        end,
       },
-      highlight = {
-        enable = true,
+      move = {
+        set_jumps = true,
       },
-      indent = {
-        enable = true,
+    },
+    keys = {
+      -- Select: parameter
+      {
+        mode = { "x", "o" },
+        "ia",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner")
+        end,
+        desc = "Select inside a parameter",
       },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-Space>",
-          node_incremental = "<C-Space>",
-          scope_incremental = false,
-          node_decremental = "<BS>",
-        },
+      {
+        mode = { "x", "o" },
+        "aa",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer")
+        end,
+        desc = "Select around a parameter",
       },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["aa"] = {
-              query = "@parameter.outer",
-              desc = "Select around a parameter",
-            },
-            ["ia"] = {
-              query = "@parameter.inner",
-              desc = "Select inside a parameter",
-            },
-            ["af"] = {
-              query = "@function.outer",
-              desc = "Select around a function",
-            },
-            ["if"] = {
-              query = "@function.inner",
-              desc = "Select inside a function",
-            },
-            ["ac"] = {
-              query = "@class.outer",
-              desc = "Select around a class",
-            },
-            ["ic"] = {
-              query = "@class.inner",
-              desc = "Select inside a class",
-            },
-          },
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg "@function.inner"
-          -- * method: eg "v" or "o"
-          -- and should return the mode ("v", "V", or "<c-v>") or a table
-          -- mapping query_strings to modes.
-          selection_modes = {
-            ["@parameter.outer"] = "v",
-            ["@parameter.inner"] = "v",
-            ["@function.outer"] = "V",
-            ["@function.inner"] = "V",
-            ["@class.outer"] = "V",
-            ["@class.inner"] = "V",
-          },
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg "@function.inner"
-          -- * selection_mode: eg "v"
-          -- and should return true of false.
-          include_surrounding_whitespace = function(params)
-            local yes = {
-              "@parameter.outer",
-              "@function.outer",
-              "@class.outer",
-            }
-            return vim.tbl_contains(yes, params.query_string)
-          end
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["]a"] = "@parameter.outer",
-            ["]f"] = "@function.outer",
-            ["]c"] = "@class.outer",
-          },
-          goto_next_end = {
-            ["]A"] = "@parameter.outer",
-            ["]F"] = "@function.outer",
-            ["]C"] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[a"] = "@parameter.outer",
-            ["[f"] = "@function.outer",
-            ["[c"] = "@class.outer",
-          },
-          goto_previous_end = {
-            ["[A"] = "@parameter.outer",
-            ["[F"] = "@function.outer",
-            ["[C"] = "@class.outer",
-          },
-          goto_next = {},
-          goto_previous = {},
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<Leader>sa"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<Leader>sA"] = "@parameter.inner",
-          },
-        },
-        lsp_interop = {
-          enable = true,
-          border = "none",
-          floating_preview_opts = {},
-          peek_definition_code = {
-            ["<Leader>pf"] = "@function.outer",
-            ["<Leader>pc"] = "@class.outer",
-          },
-        },
+      -- Select: function
+      {
+        mode = { "x", "o" },
+        "if",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@function.inner")
+        end,
+        desc = "Select inside a function",
+      },
+      {
+        mode = { "x", "o" },
+        "af",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@function.outer")
+        end,
+        desc = "Select around a function",
+      },
+      -- Select: class
+      {
+        mode = { "x", "o" },
+        "ic",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@class.inner")
+        end,
+        desc = "Select inside a class",
+      },
+      {
+        mode = { "x", "o" },
+        "ac",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@class.outer")
+        end,
+        desc = "Select around a class",
+      },
+      -- Move: parameter
+      {
+        mode = { "n", "x", "o" },
+        "]a",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_start("@parameter.outer")
+        end,
+        desc = "Jump to next start of a parameter",
+      },
+      {
+        mode = { "n", "x", "o" },
+        "[a",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_start("@parameter.outer")
+        end,
+        desc = "Jump to previous start of a parameter",
+      },
+      {
+        mode = { "n", "x", "o" },
+        "]A",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_end("@parameter.outer")
+        end,
+        desc = "Jump to next end of a parameter",
+      },
+      {
+        mode = { "n", "x", "o" },
+        "[A",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_end("@parameter.outer")
+        end,
+        desc = "Jump to previous end of a parameter",
+      },
+      -- Move: function
+      {
+        mode = { "n", "x", "o" },
+        "]f",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer")
+        end,
+        desc = "Jump to next start of a function",
+      },
+      {
+        mode = { "n", "x", "o" },
+        "[f",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer")
+        end,
+        desc = "Jump to previous start of a function",
+      },
+      {
+        mode = { "n", "x", "o" },
+        "]F",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_end("@function.outer")
+        end,
+        desc = "Jump to next end of a function",
+      },
+      {
+        mode = { "n", "x", "o" },
+        "[F",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_end("@function.outer")
+        end,
+        desc = "Jump to previous end of a function",
+      },
+      -- Move: class
+      {
+        mode = { "n", "x", "o" },
+        "]c",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer")
+        end,
+        desc = "Jump to next start of a class",
+      },
+      {
+        mode = { "n", "x", "o" },
+        "[c",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer")
+        end,
+        desc = "Jump to previous start of a class",
+      },
+      {
+        mode = { "n", "x", "o" },
+        "]C",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_end("@class.outer")
+        end,
+        desc = "Jump to next end of a class",
+      },
+      {
+        mode = { "n", "x", "o" },
+        "[C",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_end("@class.outer")
+        end,
+        desc = "Jump to previous end of a class",
+      },
+      -- Swap: parameter
+      {
+        mode = "n",
+        "<Leader>sa",
+        function()
+          require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
+        end,
+        desc = "Swap parameter with next parameter",
+      },
+      {
+        mode = "n",
+        "<Leader>sA",
+        function()
+          require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.inner")
+        end,
+        desc = "Swap parameter with previous parameter",
       },
     },
   },
