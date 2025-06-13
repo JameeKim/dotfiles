c41362d71c62f3ef77224f170a6562fe6ee8a209
Jamee Kim <jameekim@nodaplife.me>
2025-06-13 17:37:28 -0400

    feat(ft.markdown): useful wrap options

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,2 +1,5 @@
 -- Enable wrap because some markdown renderers don't ignore newlines.
 vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
+vim.opt_local.breakindentopt = { "list:-1" }
