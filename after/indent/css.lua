5415405b9be2494f6aecb2c09148e888baf307e5
Jamee Kim <jameekim@nodaplife.me>
2025-06-16 16:53:12 -0400

    fix(ft.css): move indent options to indent dir

--- after/indent/css.lua
+++ after/indent/css.lua
@@ -4,5 +4,5 @@ vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
 
 -- Revert settings.
-vim.b.undo_ftplugin = (vim.b.undo_ftplugin or "")
+vim.b.undo_indent = (vim.b.undo_indent or "")
   .. " | setl et< ts< sw<"
