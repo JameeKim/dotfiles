4da4855304d7e9d7d6328488206147213f25340e
Jamee Kim <jameekim@nodaplife.me>
2025-12-12 16:57:34 -0500

    chore(cleanup): remove unused modules.nix file

--- home/modules.nix
+++
@@ -1,7 +0,0 @@
-# Shareable modules for home-manager.
-# This file should be a plain attr set of paths to modules.
-# Directly imported to `homeModules` in flake.
-{
-  archLinux = ./arch-linux;
-  features = ./features;
-}
