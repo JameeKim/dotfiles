36f12948f4c6e94d3bef19279a0cb04d8e529c9c
Jamee Kim <jameekim@nodaplife.me>
2026-06-04 19:45:59 -0400

    feat(dev.llm): disable OpenCode LSP by default

--- home/jameekim/dev/llm/default.nix
+++ home/jameekim/dev/llm/default.nix
@@ -13,6 +13,8 @@ in
   config = lib.mkIf (devCfg.enable && cfg.enable) {
     programs.opencode = {
       enable = true;
+      # TODO: Go through permissions.
+      # TODO: Read access to external paths like cargo src.
       settings = lib.importJSON ./opencode/opencode.json;
       tui = lib.importJSON ./opencode/tui.json;
     };
