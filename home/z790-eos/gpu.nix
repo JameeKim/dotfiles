a71faa9726bbaf06c741dffea23874d45d4c2058
Jamee Kim <jameekim@nodaplife.me>
2026-09-17 16:44:14 -0400

    chore(nvidia): update on z790-eos

--- home/z790-eos/gpu.nix
+++ home/z790-eos/gpu.nix
@@ -8,8 +8,8 @@ let
   # with the version string as an argument.
   # ex) ./scripts/fetch-nvidia-driver.sh 580.105.08
   nvidia = {
-    version = "610.43.03";
-    sha256 = "sha256-ReLUwTSiPDXlDyU6SqY+fl6NF+PRhdSgfIpY6WEu05I=";
+    version = "615.71.09";
+    sha256 = "sha256-zc7tIrvrYSSNGm3qvCWWZz46ZQFpjucayNL9wo87cP4=";
   };
 in
 {
